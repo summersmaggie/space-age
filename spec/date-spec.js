35f394bead7dd7807dea449a399c66241b9a4a06
Maggie Summers <summers.maggie@gmail.com>
2018-03-16 09:09:56 -0700

    write second test spec

--- spec/date-spec.js
+++ spec/date-spec.js
@@ -5,4 +5,9 @@ describe('Date', function() {
     let newDate = new Date("1988-03-09")
     expect(newDate.birthdate).toEqual("1988-03-09")
   });
+
+  it('takes an age and converts it to seconds', function() {
+    let newDate = new Date("1988-03-09")
+    expect(newDate.yearsToSeconds()).toEqual("946,708,560")
+  });
 });
